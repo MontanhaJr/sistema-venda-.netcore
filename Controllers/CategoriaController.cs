ec32876489ea76a9f5db102429808bb47351ce44
Mauricio Montanha Junior <mauriciomontanhajunior@gmail.com>
2021-04-29 15:16:55 -0300

    Criadas as views

---
+++ Controllers/CategoriaController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaVenda.DAL;
+using SistemaVenda.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaVenda.Controllers
+{
+    public class CategoriaController : Controller
+    {
+        protected ApplicationDbContext mContext;
+
+        public CategoriaController(ApplicationDbContext context)
+        {
+            mContext = context;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Categoria> lista = mContext.Categoria.ToList();
+            mContext.Dispose();
+            return View(lista);
+        }
+
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+    }
+}
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using SistemaVenda.DAL;
+using SistemaVenda.Entidades;
 using SistemaVenda.Models;
 using System;
 using System.Collections.Generic;
@@ -12,11 +14,13 @@ namespace SistemaVenda.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        protected ApplicationDbContext Repositorio;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext repositorio)
         {
             _logger = logger;
-        }
+            Repositorio = repositorio;
+        }       
 
         public IActionResult Index()
         {
