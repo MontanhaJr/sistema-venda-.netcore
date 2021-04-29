8844c8d4f5d471fcac362f131c38777d5fada087
Mauricio Montanha Junior <mauriciomontanhajunior@gmail.com>
2021-04-29 16:50:34 -0300

    Cadastro de Categorias

--- Controllers/CategoriaController.cs
+++ Controllers/CategoriaController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SistemaVenda.DAL;
 using SistemaVenda.Entidades;
+using SistemaVenda.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,9 +25,60 @@ namespace SistemaVenda.Controllers
             return View(lista);
         }
 
-        public IActionResult Cadastro()
+        [HttpGet]
+        public IActionResult Cadastro(int? id)
         {
-            return View();
+            CategoriaViewModel viewModel = new CategoriaViewModel();
+
+            if(id != null)
+            {
+                var entidade = mContext.Categoria.Where(x => x.Codigo == id).FirstOrDefault();
+                viewModel.Codigo = entidade.Codigo;
+                viewModel.Descricao = entidade.Descricao;
+            }
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Cadastro(CategoriaViewModel entidade)
+        {
+            if(ModelState.IsValid)
+            {
+                Categoria objCategoria = new Categoria()
+                {
+                    Codigo = entidade.Codigo,
+                    Descricao = entidade.Descricao
+                };
+
+                if(entidade.Codigo == null)
+                {
+                    mContext.Categoria.Add(objCategoria);
+                }
+                else
+                {
+                    mContext.Categoria.Update(objCategoria);
+                }
+
+                mContext.SaveChanges();
+            }
+            else
+            {
+                return View(entidade);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Excluir(int id)
+        {
+            var objCategoria = mContext.Categoria.Where(x => x.Codigo == id).FirstOrDefault();
+
+            mContext.Remove(objCategoria);
+            mContext.SaveChanges();
+         
+            return RedirectToAction("Index");
         }
     }
 }
