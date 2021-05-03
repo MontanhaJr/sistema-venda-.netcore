a35627c8c346965b89ad9625309b6123d08e9d45
Mauricio Montanha Junior <mauriciomontanhajunior@gmail.com>
2021-05-03 11:43:56 -0300

    Criado o Login

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaVenda.DAL;
+using SistemaVenda.Helpers;
+using SistemaVenda.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaVenda.Controllers
+{
+    public class LoginController : Controller
+    {
+        protected ApplicationDbContext mContext;
+
+        public LoginController(ApplicationDbContext context)
+        {
+            mContext = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(LoginViewModel model)
+        {
+            ViewData["ErroLogin"] = string.Empty;
+            if (ModelState.IsValid)
+            {
+                var autenticacao = mContext.Usuario.Where(x => 
+                                x.Email == model.Email && 
+                                x.Senha == Criptografia.GetMd5Hash(model.Senha))
+                                .FirstOrDefault();
+
+                if (autenticacao == null)
+                {
+                    ViewData["ErroLogin"] = "Email e/ou Senha incorretos";
+                    return View(model);
+                }
+                else
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            else
+            {
+                return View(model);
+            }
+
+        }
+    }
+}
