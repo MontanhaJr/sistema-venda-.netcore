0998eddcf8f13d77588b867ff0a2cc0d02722aef
Mauricio Montanha Junior <mauriciomontanhajunior@gmail.com>
2021-05-11 13:44:30 -0300

    Alterar local do repositorio

---
+++ Repositorio/Entidades/RepositorioVendaProdutos.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Repositorio.Contexto;
+using Repositorio.Repositorio;
+using SistemaVenda.Dominio.DTO;
+using SistemaVenda.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Repositorio.Entidades
+{
+    public class RepositorioVendaProdutos : IRepositorioVendaProdutos
+    {
+        protected ApplicationDbContext DbSetContext;
+        public RepositorioVendaProdutos(ApplicationDbContext mContext)
+        {
+            DbSetContext = mContext;
+        }
+
+        public IEnumerable<GraficoViewModel> ListaGrafico()
+        {
+            var lista = (from r in DbSetContext.VendaProdutos.Include(x => x.Produto)
+                         group r by new { r.CodigoProduto, r.Produto.Descricao}
+                         into g
+                         select new GraficoViewModel
+                         {
+                             CodigoProduto = g.Key.CodigoProduto,
+                             Descricao = g.Key.Descricao,
+                             TotalVendido = g.Sum(z => z.Quantidade)
+                         }).ToList();
+
+            return lista;
+        }
+    }
+}
---
+++ Repositorio/Repositorio.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Repositorio.Interfaces;
+using System;
+using SistemaVenda.Dominio.Entidades;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Dominio.Entidades;
+
+namespace Repositorio
+{
+    public abstract class Repositorio<TEntidade> : DbContext, IRepositorio<TEntidade> where TEntidade : EntityBase, new()
+    {
+        protected DbContext Db;
+        protected DbSet<TEntidade> DbSetContext;
+        public Repositorio(DbContext dbContext)
+        {
+            Db = dbContext;
+            DbSetContext = Db.Set<TEntidade>();
+        }
+
+        public void Create(TEntidade Entity)
+        {
+            if(Entity.Codigo == null)
+            {
+                DbSetContext.Add(Entity);
+            }
+            else
+            {
+                DbSetContext.Update(Entity);
+            }
+
+            Db.SaveChanges();
+        }
+
+        public virtual void Delete(int id)
+        {
+            TEntidade ent = DbSetContext.Where(x => x.Codigo == id).FirstOrDefault();
+            DbSetContext.Remove(ent);
+            Db.SaveChanges();
+        }
+
+        public TEntidade Read(int id)
+        {
+            return DbSetContext.Where(x => x.Codigo == id).FirstOrDefault();
+        }
+
+        public virtual IEnumerable<TEntidade> Read()
+        {
+            return DbSetContext.AsNoTracking().ToList();
+        }
+    }
+}
